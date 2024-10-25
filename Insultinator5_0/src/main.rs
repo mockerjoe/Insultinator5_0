4d61d6df17bc4278a24d55c028856dc625df09c7
JANNIK-PC\Jannik <jannik.vogelpohl@kabelmail.de>
2024-10-25 21:31:50 +0200

    inserted first insult to main.rs

--- Insultinator5_0/src/main.rs
+++ Insultinator5_0/src/main.rs
@@ -1,3 +1,29 @@
+use enigo::{
+    Button, Coordinate,
+    Direction::{Click, Press, Release},
+    Enigo, Key, Keyboard, Mouse, Settings,
+};
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut enigo = Enigo::new(&Settings::default()).unwrap();
+    // Open chat Key
+    enigo.key(Key::Unicode('y'), Click);
+
+    thread::sleep(Duration::from_millis(500));
+
+    // Insert text in chatbox
+
+    enigo.text("Huansohn");
+
+    thread::sleep(Duration::from_millis(500));
+
+    // Simulate pressing the Enter key
+
+    enigo.key(Key::Return, Press); 
+
+    thread::sleep(Duration::from_millis(500));
+
+    enigo.key(Key::Return, Release);
 }
